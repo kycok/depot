b9311620e38b8b6ca9ff203cf59df7b156b7f02e
kycok <abocharnikov@gmail.com>
2009-08-20 10:18:13 +0400

    3 commit

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090813102240) do
+ActiveRecord::Schema.define(:version => 20090820060045) do
 
   create_table "products", :force => true do |t|
     t.string   "title"
